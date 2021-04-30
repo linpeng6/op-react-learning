addab61cac5d3fd53ac23e4d9c9e2232df1d45c6
linpeng <linpeng1@apexsoft.com.cn>
2021-04-30 09:08:55 +0800

    fiex:inputValue null

--- react-redux/src/App.js
+++ react-redux/src/App.js
@@ -1,39 +1,48 @@
 import React, { Component } from 'react'
 
 export default class App extends Component {
-   constructor(props) {
-       super(props);
-       //获取store中的state
-       this.state=props.store.getState();
-   }
 
-   changeValue(e){
-       
+    
+    constructor(props) {
+        super(props);
+
+        const {store} = props;
+
+        //获取store中的state
+        this.state = store.getState();
+        
+        store.subscribe(()=>{ 
+            this.setState(store.getState());
+        })
+    }
+
+    changeValue(e) {
+
         const action = {
-            type:'changeInput',
-            data:e.target.value
+            type: 'changeInput',
+            data: e.target.value
         }
         this.props.store.dispatch(action)
-   }
-   
-   add(){
-        this.props.store.dispatch({type:'addItem'})
-   }
+    }
 
-   deleteItem(index){
-       
-        this.props.store.dispatch({type:'deleteItem',data:index})
-   }
+    add() {
+        this.props.store.dispatch({ type: 'addItem' })
+    }
+
+    deleteItem(index) {
+
+        this.props.store.dispatch({ type: 'deleteItem', data: index })
+    }
     render() {
         return (
             <div>
-                <input onChange={(e)=>{this.changeValue(e)}} />
-                <button onClick={()=>{this.add()}}>增加</button>
+                <input onChange={e => { this.changeValue(e) }} value={this.state.inputValue}/>
+                <button onClick={() => { this.add() }}>增加</button>
                 <ul>
-                    {this.state.list.map((item,index)=>{ 
+                    {this.state.list.map((item, index) => {
                         return (
-                            <li key={index} onClick={()=>{this.deleteItem(index)}}>
-                            {item}
+                            <li key={index} onClick={() => { this.deleteItem(index) }}>
+                                {item}
                             </li>
                         )
                     })}
--- react-redux/src/index.js
+++ react-redux/src/index.js
@@ -3,15 +3,11 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import store from './redux/store'
 
-function render(){
-  ReactDOM.render(
+
+ReactDOM.render(
     <React.StrictMode>
       <App store={store}/>
     </React.StrictMode>,
     document.getElementById('root')
-  );
-}
+);
 
-render()
-
-store.subscribe(render)
